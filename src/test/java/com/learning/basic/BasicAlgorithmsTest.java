7dae3407fd9c1a61a81c6a93ba552d96d6519457
Joao Vale <joao.vale@retarus.pt>
2023-09-27 16:29:20 +0100

    refactor

--- src/test/java/com/learning/basic/BasicAlgorithmsTest.java
+++ src/test/java/com/learning/basic/BasicAlgorithmsTest.java
@@ -3,37 +3,18 @@ package com.learning.basic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-import java.time.Duration;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertLinesMatch;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BasicAlgorithmsTest {
 
-    @Test
-    void helloWorldTest() {
-        System.out.println("Hello World");
-    }
+    private final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
 
     @Test
     void isEvenReturnsTrueTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int evenNumber = 222;
         final boolean expected = true;
 
@@ -48,7 +29,6 @@ class BasicAlgorithmsTest {
     @Test
     void isEvenReturnsFalseTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int evenNumber = 111;
         final boolean expected = false;
 
@@ -63,7 +43,6 @@ class BasicAlgorithmsTest {
     @Test
     void isPrimeReturnsTrueTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int number = 11;
 
         // when
@@ -77,7 +56,6 @@ class BasicAlgorithmsTest {
     @Test
     void isPrimeReturnsFalseTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int number = 432;
 
         // when
@@ -91,7 +69,6 @@ class BasicAlgorithmsTest {
     @Test
     void isPrimeReturnsFalseToZeroValueTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int number = 0;
 
         // when
@@ -105,7 +82,6 @@ class BasicAlgorithmsTest {
     @Test
     void isPrimeReturnsFalseToNegativeValueTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int number = -11;
 
         // when
@@ -115,39 +91,10 @@ class BasicAlgorithmsTest {
         assertFalse(actual);
     }
 
-    @DisplayName("example of assertions")
-    @Test
-    void assertionsExamplesTest() {
-        // given
-        var someObject = BigDecimal.valueOf(100);
-        var anotherObject = BigDecimal.valueOf(100);
-        var basicAlgorithm = new BasicAlgorithms();
-
-        // when
-        // then
-        assertTrue(true);
-        assertFalse(false);
-        assertNull(null);
-        assertNotNull(someObject);
-        assertEquals(someObject, anotherObject, "someObject is not equals to anotherObject");
-        assertArrayEquals(new int[]{1, 3, 5}, new int[]{1, 3, 5});
-        assertLinesMatch(List.of("line 1", "line 2", "line 3"), List.of("line 1", "line 2", "line 3"));
-        assertNotEquals("some string", "another string");
-        assertSame(someObject, someObject);
-        assertNotSame(someObject, anotherObject);
-        assertThrowsExactly(ArithmeticException.class, () -> {int x = 1 / 0;});
-        assertThrows(RuntimeException.class, () -> {var x = 1 / 0;});
-        assertDoesNotThrow(() -> {var x = 1/0.0;});
-        assertTimeout(Duration.ofSeconds(3), () -> {basicAlgorithm.isPrime(34567890);});
-        assertInstanceOf(BigDecimal.class, someObject);
-        //fail("Method failed");
-    }
-
     @DisplayName("given a positive number, when factorial is called, then returns its value")
     @Test
     void factorialTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int number = 5;
         final int expected = 120;
 
@@ -162,7 +109,6 @@ class BasicAlgorithmsTest {
     @Test
     void factorialExceptionTest() {
         // given
-        final BasicAlgorithms basicAlgorithms = new BasicAlgorithms();
         final int number = -1;
 
         // when
